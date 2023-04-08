d3c7a24e46a54d2605c98026c9a08c772a35fa88
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2023-04-08 16:13:00 +0900

    Add files via upload

--- myFirstGame_Run/TitleUI.cs
+++ myFirstGame_Run/TitleUI.cs
@@ -12,8 +12,11 @@ public class TitleUI : MonoBehaviour
 
     // +++ 해상도 관련 변수 +++
     // FullScreenMode 열거형
-    FullScreenMode screenMode;
-    public Toggle fullScreenBtn;
+    FullScreenMode screenMode; 
+    // 창모드 여부
+    bool isWindow;
+
+    public Toggle windowModeToggle; // 창모드 토글 
     // options에 값을 넣어주려면 Dropdown 변수를 선언해줘야 한다.
     public Dropdown resolutionDropdown;
     // DropboxOptionChange(int x)에서 x
@@ -21,19 +24,37 @@ public class TitleUI : MonoBehaviour
     // 지원하는 해상도를 넣어줄 리스트 생성 
     List<Resolution> resolutions = new List<Resolution>();
 
+
     // 게임 스타트를 누르면 스테이지 선택 씬으로 넘어감
-    // 스테이지 선택씬의 이름을 담는 변
+    // 스테이지 선택씬의 이름을 담는 변수 
     public string selectStage;
 
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        options.SetActive(false); // 패널숨기기
-        InitUI();
+        // PlayerPrefs.SetInt("screen rex index", resolutionNum);
+        resolutionNum = PlayerPrefs.GetInt("screen res index");
+        // PlayerPrefs.SetInt("WindowMode", ((isWindow) ? 1 : 0));
+        isWindow = (PlayerPrefs.GetInt("WindowMode") == 1) ? true : false;
+
+        windowModeToggle.isOn = isWindow;
     }
 
-    void InitUI()
+    void Start()
     {
+        // 옵션 패널숨기기
+        options.SetActive(false);
+        InitResolutionDropdown();
+    }
+
+    // 해상도 드롭다운의 값을 모니터 지원 해상도 값 목록으로 초기화해주는 함/
+    void InitResolutionDropdown()
+    {
+
+        // Dropdown에 있는 기존의 옵션을 전부 제거
+        // options는 리스트이기 때문에 리스트의 Clear()함수를 사용하여 전부 제거할 수 있다.
+        resolutionDropdown.options.Clear();
+
         // 모니터가 지원하는 해상도는 Screen.resolutions라는 배열에 들어있다.
             // AddRange함수를 이용해서 지원하는 함수를 전부 리스트에 넣을 수 있다.
             // resolutions.AddRange(Screen.resolutions);
@@ -43,14 +64,11 @@ public class TitleUI : MonoBehaviour
             if (Screen.resolutions[i].refreshRate == 60)
                 resolutions.Add(Screen.resolutions[i]);
         }
-        // Dropdown에 있는 기존의 옵션을 전부 제거
-        // options는 리스트이기 때문에 리스트의 Clear()함수를 사용하여 전부 제거할 수 있다.
-        resolutionDropdown.options.Clear();
-
-
+        
         int optionNum = 0;
 
-        // 반복문에서 가져온 해상도의 개수만큼 반복하면서 option 추가
+        // 위에서 지원하는 해상도를 resolutions 리스트에 넣었다 
+        // 리스트의 길이 만큼 반복하면서 option 추가
         foreach (Resolution item in resolutions)
         {
             // options list의 형식은 OptionData 클래스로 되어 있다 -> OptionData 객체를 생성
@@ -74,7 +92,7 @@ public class TitleUI : MonoBehaviour
 
         // 현재 전체화면인지 아닌지 확인 후 토글 버튼의 체크 버튼을 초기화
         // 창모드라면 창모드 토글에 체크
-        fullScreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.Windowed) ? true : false;
+        // windowModeToggle.isOn = Screen.fullScreenMode.Equals(FullScreenMode.Windowed) ? true : false;
     }
 
     // Dropbox의 value값을 이용하여 현재 선택된 해상도를 적용할 수 있게 하는 메서드
@@ -94,7 +112,9 @@ public class TitleUI : MonoBehaviour
 
     public void OnClick_Options()
     {
-        options.SetActive(true); // Options 패널 활성화 
+        // Options 패널 활성화, title 패널 끄기 
+        options.SetActive(true);
+        title.SetActive(false);
     }
 
     // Title에서 QUIT버튼을 누르면 게임 종료 
@@ -103,13 +123,18 @@ public class TitleUI : MonoBehaviour
         Application.Quit();
     }
 
-    //OptionUI Input
 
+
+    //OptionUI Input
     // 창모드 토글 버튼 
-    public void FullScreenBtn(bool isWindow)
+    public void WindowModeBtn(bool isWindowOn)
     {
+        // 버튼이 클릭되면 isWindow 변수를 변경
+        isWindow = isWindowOn;
+
         // 참이면 창모드 / 거짓이면 전체화면
         screenMode = isWindow ? FullScreenMode.Windowed : FullScreenMode.FullScreenWindow;
+
     }
 
     // 오른쪽 아래 확인 버튼
@@ -120,7 +145,17 @@ public class TitleUI : MonoBehaviour
         Screen.SetResolution(resolutions[resolutionNum].width,
             resolutions[resolutionNum].height,
             screenMode);
-        options.SetActive(false); // 패널숨기기
+
+        // 해상도랑 창모드를 PlayerPrefs에 저장 
+        PlayerPrefs.SetInt("screen rex index", resolutionNum);
+        PlayerPrefs.Save();
+
+        PlayerPrefs.SetInt("WindowMode", ((isWindow) ? 1 : 0));
+        PlayerPrefs.Save();
+
+        // 옵션 패널 끄고 타이틀 패널 열기 
+        options.SetActive(false);
+        title.SetActive(true);
     }
 
 
